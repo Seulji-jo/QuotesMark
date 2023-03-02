2a41c01c9ea3ff87f730f60a9b7fbb7b7b38f8fb
Seulji-jo <joseuling@gmail.com>
2023-03-03 02:03:15 +0900

    Create ErrorPage
    - 404등 에러를 보여주기 위한 페이지 생성
    - routesConfig에 errorElements에 추가

--- src/config/routesConfig.js
+++ src/config/routesConfig.js
@@ -4,11 +4,13 @@ import { Navigate } from 'react-router-dom';
 const Layout = lazy(() => import('../layouts'));
 const KoreanQuotes = lazy(() => import('../pages/KoreanQuotes'));
 const EnglishQuotes = lazy(() => import('../pages/EnglishQuotes'));
+const ErrorPage = lazy(() => import('../pages/ErrorPage'));
 
 const routesConfig = [
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
