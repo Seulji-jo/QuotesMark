6322ac50ff95aec0ea40c1f48e7f8d1eed51d2a0
Seulji-jo <joseuling@gmail.com>
2023-03-02 20:57:10 +0900

    Create KoreanPage and EnglishPage
    - 현재 페이지 파일만 생성해놓은 상태
    
    - Close: #2

---
+++ src/pages/EnglishQuotes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function EnglishQuotes() {
+  return <div>English Quotes</div>;
+}
+
+export default EnglishQuotes;
---
+++ src/pages/KoreanQuotes.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function KoreanQuotes() {
+  return <div>Korean Quotes</div>;
+}
+
+export default KoreanQuotes;
