f239448f9d39f9bf44f64827f4f7ae3d40f4d598
Seulji-jo <joseuling@gmail.com>
2023-03-03 03:31:54 +0900

    Setup Header
    - Header에 필요한 내용 셋업
      - 로고, 각 페이지로 이동할 네비게이션

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../assets/Logo.svg';
 
 function Header() {
-  return <header>Header</header>
+  let activeClassName = 'underline';
+  return (
+    <header>
+      <Logo />
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="kr"
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              KOR
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="en"
+              className={({ isActive }) =>
+                isActive ? activeClassName : undefined
+              }
+            >
+              ENG
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
